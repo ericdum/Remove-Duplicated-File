cbd4c502753fdc72c97576df6729be24f7e2af49
senhong.ssh <senhong.ssh@alibaba-inc.com>
2020-04-11 11:16:36 +0800

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const { program } = require('commander');
+const chalk = require('chalk');
+const clearTheDir = require('./lib/clear');
+const path = require('path'); const fs = require('fs-extra');
+
+program
+  .arguments('<targetDir> <trashDir>')
+  .action(async function (targetDir, trashDir) {
+    targetDir = await ensureDir(targetDir)
+    trashDir = await ensureDir(trashDir, true)
+
+    console.log(chalk.blue('[Start] to clean:'), targetDir, chalk.yellow('Save to: '+ trashDir))
+    await clearTheDir(targetDir, trashDir)
+  });
+
+program.parse(process.argv);
+
+async function ensureDir(dir, ensure){ 
+  if (!path.isAbsolute(dir))
+    dir = path.join(process.cwd(), dir);
+
+  if (ensure) await fs.ensureDir(dir)
+  var stats = fs.statSync(dir)
+  if (!stats.isDirectory()) {
+    return console.log(dir, chalk.red('is not a directory'))
+  }
+  return dir;
+}
